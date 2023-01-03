8577c284d4ac5f56f9dc97b277d9fe7fc24fea7f
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-03 14:29:40 +0700

    bracket combination answer

---
+++ Coderbyte/BracketCombination.js
@@ -0,0 +1,24 @@
+/* 
+BracketCombinations(num) read num which will be an integer greater than or equal to zero, and return the number of valid combinations that can be formed with num pairs of parentheses. For example, if the input is 3, then the possible combinations of 3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()), (())(), ((())), and (()()). There are 5 total combinations when the input is 3, so your program should return 5.
+Examples
+Input: 3
+Output: 5
+Input: 2
+Output: 2
+*/
+
+function BracketCombinations(num) { 
+
+    // code goes here  
+   let factorial = (n) => {
+    let m = 1;
+    for(let i = n; i >= 1; i--) {
+        m *= i
+    }
+    return m
+   }
+  const result = ()
+  }
+     
+  // keep this function call here 
+  console.log(BracketCombinations(readline()));
