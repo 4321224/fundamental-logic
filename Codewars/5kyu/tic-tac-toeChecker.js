0050b23e5c21663fab49c33f9f20e181b5a899ea
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-02-15 10:47:51 +0700

    passed test tic tac toe checker

--- Codewars/5kyu/tic-tac-toeChecker.js
+++ Codewars/5kyu/tic-tac-toeChecker.js
@@ -55,11 +55,17 @@ function isSolved(board) {
   }
 
   // Check if the board is full
+  let isFull = true;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (board[i][j] === 0) {
-        return -1;
+        isFull = false;
+        break;
       }
+    }
+    if (!isFull) {
+      // The game is not finished
+      return -1;
     }
   }
 
